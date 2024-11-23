c5518b904773e8e57ca812bed44c95c6c77bc475
iSimpp <103945070+iSimpp@users.noreply.github.com>
2024-11-23 16:59:36 +0100

    MAJOR UPDATE!! Ability to Import/Export, still needs to be modified a little bit to work, but...

--- Export/Import.py
+++ Export/Import.py
@@ -4,23 +4,25 @@ import sys
 import Library
 import requests
 
+temp = os.path.expandvars('%APPDATA%\.temp')
+
 def unzip_file(zip_path):
     # Ensure the temp directory exists
-    if not os.path.isdir('temp'):
-        os.mkdir('temp')
+    if not os.path.isdir(temp):
+        os.mkdir(temp)
 
     # Extract the files from the zip
     with zipfile.ZipFile(zip_path, 'r') as zip_ref:
-        zip_ref.extractall('temp')
+        zip_ref.extractall(temp)
 
     # Get the full path to the temp directory
-    temp_dir = os.path.abspath('temp')
+    temp_dir = os.path.abspath(temp)
 
     # Iterate through the files in the temp directory
     for file in os.listdir(temp_dir):
         file_path = os.path.join(temp_dir, file)  # Get the full file path
         if os.path.isfile(file_path):  # Check if it is a file
-            destination = os.path.expandvars(r'%APPDATA%\\.minecraft\mods')
+            destination = os.path.expandvars(r'%APPDATA%\.minecraft\mods')
             # Move the file to the destination
             os.rename(file_path, os.path.join(destination, file))  # Renaming to move the file
 
